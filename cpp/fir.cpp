0e5af8656abbe9a7e0fbcb1e29c173832d445a6b
scast_lx <santiagoc333@gmail.com>
2026-08-22 17:48:46 -0400

    added x

--- cpp/fir.cpp
+++ cpp/fir.cpp
@@ -3,23 +3,26 @@
 
 class FIR {
 private:
-	std::vector<double> b;
+	std::vector<double> h;
+	std::vector<double> x;
 	size_t num_taps;
 
 public:
-	FIR(const std::vector<double>& coeffs) : b(coeffs), num_taps(coeffs.size()) {}
+	FIR(const std::vector<double>& coeffs) : h(coeffs), x(coeffs.size(), 0.0), num_taps(coeffs.size()) {}
+
 
 	double process(double in_sample){
-
+		for (int i = 0; i<num_taps; i++){
+			
 		return 0;
 	}
 
 
-}
+};
 
 int main(int argc, char* argv[]){
 
-
+	std::vector<double> matlab_coeffs; //insert from tx file
 
 
 	return 0;
